ce1764243093b8f21e56399423b476ef97f93233
Maria Sofia Perez <perezmariasofia@gmail.com>
2025-06-30 20:06:41 -0300

    mejorando entidad, agregando dto

--- src/main/java/com/tatutaller/dto/request/BookingRequest.java
+++ src/main/java/com/tatutaller/dto/request/BookingRequest.java
@@ -11,10 +11,17 @@ public class BookingRequest {
     @NotNull(message = "La fecha es obligatoria")
     private LocalDate bookingDate;
 
-    @NotNull(message = "La hora es obligatoria")
-    private LocalTime bookingTime;
+    @NotNull(message = "La hora de inicio es obligatoria")
+    private LocalTime starTime;
 
-    private String notes; // Notas adicionales del cliente
+    @NotNull(message = "La hora de fin es obligatoria")
+    private LocalTime endTime;
+
+    @NotNull(message = "El tipo de reserva es obligatorio")
+    private String bookingType; // "PUNTUAL" O "RECURRENTE"
+
+    private LocalDate recurrenceEndDate;
+    private String notes;
 
     // Constructors
     public BookingRequest() {
@@ -33,16 +40,40 @@ public class BookingRequest {
         return bookingDate;
     }
 
+    public LocalTime getStarTime() {
+        return starTime;
+    }
+
+    public void setStarTime(LocalTime starTime) {
+        this.starTime = starTime;
+    }
+
+    public LocalTime getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(LocalTime endTime) {
+        this.endTime = endTime;
+    }
+
+    public String getBookingType() {
+        return bookingType;
+    }
+
+    public void setBookingType(String bookingType) {
+        this.bookingType = bookingType;
+    }
+
+    public LocalDate getRecurrenceEndDate() {
+        return recurrenceEndDate;
+    }
+
+    public void setRecurrenceEndDate(LocalDate recurrenceEndDate) {
+        this.recurrenceEndDate = recurrenceEndDate;
+    }
+
     public void setBookingDate(LocalDate bookingDate) {
         this.bookingDate = bookingDate;
-    }
-
-    public LocalTime getBookingTime() {
-        return bookingTime;
-    }
-
-    public void setBookingTime(LocalTime bookingTime) {
-        this.bookingTime = bookingTime;
     }
 
     public String getNotes() {
